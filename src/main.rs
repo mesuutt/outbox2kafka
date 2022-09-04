eea9b874ea63d1bbcdf8ddc86401b703d9aa8a55
Mesut Tasci <slackermesut@gmail.com>
2022-09-04 23:27:02 +0300

    update producer creation

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,11 @@ async fn main() -> AppResult<()> {
     env_logger::init();
 
     let opts = Opt::from_args();
-    println!("{:?}", opts.clone());
+
     let db_pool = create_pool(opts.db_url).await?;
     let repo = Repo::new(db_pool, opts.retention);
 
-    let producer = match Producer::new(opts.brokers, opts.topic, repo, opts.db_check_interval) {
-        Ok(p) => p,
-        Err(e) => return Err(e),
-    };
-
+    let producer = Producer::new(opts.brokers, opts.topic, repo, opts.db_check_interval)?;
     producer.start().await?;
 
     Ok(())
